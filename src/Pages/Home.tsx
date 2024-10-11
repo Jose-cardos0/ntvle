08f86a469b7be1d994b238c51ad81fdb6b73def5
Jose Cardoso <josedeveloperJS@gmail.com>
2024-10-11 17:59:08 -0300

    atualizacao 11-10 scroll trigger

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,18 @@
-import { useRef, useEffect } from "react";
-import { IoIosArrowDropdownCircle } from "react-icons/io";
-
+import { useLayoutEffect } from "react";
 //logo
 import campo from "../assets/campo.svg";
 import coracao from "../assets/coracao.svg";
+import bgprodutos from "../assets/bgprodutos.png";
+import integralBg from "../assets/integralBg.png";
+import zeroBg from "../assets/bglinhas/zeroBg_1.png";
+import desnBg from "../assets/bglinhas/desnBg_1.png";
+import leitesBg from "../assets/bglinhas/leites_1.png";
+import queijosBg from "../assets/bglinhas/queijos.png";
+import manteigasBg from "../assets/bglinhas/manteigas.png";
+import integralBg2 from "../assets/bglinhas/integralBg2.png";
+import leitesEmPo from "../assets/bglinhas/empo.png";
+
+import vacaBracosAbertos from "../assets/vacaBracosAbertos.svg";
 
 //slider
 import { Slider } from "../Components/Slider";
@@ -11,16 +20,34 @@ import { Slider } from "../Components/Slider";
 //3d
 import { Cheese3D } from "../Components/Cheese";
 
-export function Home() {
-  const sectionRef = useRef<HTMLDivElement | null>(null);
+//icons
+import { LiaArrowAltCircleDownSolid } from "react-icons/lia";
 
-  const handleScrollToSection = () => {
-    if (sectionRef.current) {
-      sectionRef.current.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
+//gsap
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export function Home() {
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#textoGsapQueijo3d", {
+      x: 0,
+      opacity: 1,
+      duration: 6,
+      scrollTrigger: {
+        trigger: "#startScroll",
+        markers: true,
+        start: "top 200px",
+        end: "bottom 850px",
+        scrub: true,
+      },
+    });
+
+    return () => {
+      gsap.killTweensOf("#textoGsapQueijo3d");
+    };
+  }, []);
 
   return (
     <main>
@@ -30,23 +57,39 @@ export function Home() {
        flex items-center justify-center bg-[#56bac8] flex-col relative"
       >
         <Slider />
+
         <button
-          onClick={handleScrollToSection}
-          className="bg-[#56bac8] 
-          rounded-full flex items-end
-           absolute bottom-20 right-5 animate-pulse z-50 cursor-pointer"
+          onClick={() => {
+            window.scrollTo({
+              top: window.scrollY + window.innerHeight,
+              behavior: "smooth",
+            });
+          }}
+          className=" 
+          rounded-full flex items-end animate-bounce
+          fixed bottom-10 right-5  z-50 cursor-pointer flex-col"
         >
-          <IoIosArrowDropdownCircle size={100} color="#003fa5" />
+          <div className="flex justify-center">
+            <img className="w-40 mr-5" src={vacaBracosAbertos} alt="" />
+          </div>
+          <div
+            className="flex items-center justify-center pr-4 bg-white shadow-2xl
+            rounded-full hover:bg-[#56bac8] hover:shadow-xl "
+          >
+            <LiaArrowAltCircleDownSolid size={60} color="#003fa5" />
+            <strong className="text-blue-800">VEJA MAIS</strong>
+          </div>
         </button>
       </header>
       <section
-        ref={sectionRef}
+        id="startScroll"
         className="h-3/6 
        flex items-center justify-center bg-[#56bac8] bg-no-repeat bg-right 
       "
       >
         <h1
-          className="text-blue-800 font-bold text-8xl flex items-center justify-center "
+          id="textoGsapQueijo3d"
+          className="text-blue-900 font-bold text-8xl flex items-center justify-center "
           style={{ fontFamily: "Mocha Mattari W01 Regular" }}
         >
           conheça <br /> nosso <br /> carro <br />
@@ -57,25 +100,77 @@ export function Home() {
           <Cheese3D />
         </div>
       </section>
-      <section className="min-w-full h-screen flex items-center justify-center flex-col">
+      <section
+        style={{
+          backgroundImage: `url(${bgprodutos})`,
+        }}
+        className="min-w-full 
+        h-screen flex 
+        items-center justify-center 
+        flex-col bg-cover bg-center "
+      >
         <h1
-          className="text-blue-800 font-bold text-8xl flex items-center justify-center "
-          style={{ fontFamily: "Mocha Mattari W01 Regular" }}
+          style={{
+            fontFamily: "Mocha Mattari W01 Regular",
+          }}
+          className="text-blue-900 font-bold text-8xl flex
+           items-center justify-center drop-shadow-2xl
+           "
         >
-          conheça nossos produtos
+          nossas linhas de produtos
         </h1>
-        <div className=" grid grid-cols-4 gap-6 items-center justify-center m-auto ">
-          <div className="w-96 h-96  rounded-lg shadow-xl transition duration-300 hover:scale-105 cursor-pointer">
-            <h1>QUEIJOS</h1>
+        <div
+          className=" grid grid-cols-4 gap-6
+         items-center justify-center
+          m-auto "
+        >
+          <div
+            style={{ backgroundImage: `url(${integralBg})` }}
+            className="w-96 h-96  rounded-lg shadow-xl
+           bg-cover bg-center items-center justify-center flex"
+          >
+            <img
+              className="max-w-64 transition duration-300 hover:scale-105 
+          cursor-pointer hover:drop-shadow-2xl"
+              src={leitesBg}
+              alt=""
+            />
           </div>
-          <div className="w-96 h-96  rounded-lg shadow-xl">
-            <h1>QUEIJOS</h1>
+          <div
+            style={{ backgroundImage: `url(${desnBg})` }}
+            className="w-96 h-96  rounded-lg shadow-xl
+           bg-cover bg-center items-center justify-center flex"
+          >
+            <img
+              className="max-w-96 transition duration-300 hover:scale-105 
+          cursor-pointer hover:drop-shadow-2xl"
+              src={queijosBg}
+              alt=""
+            />
           </div>
-          <div className="w-96 h-96  rounded-lg shadow-xl">
-            <h1>QUEIJOS</h1>
+          <div
+            style={{ backgroundImage: `url(${zeroBg})` }}
+            className="w-96 h-96  rounded-lg shadow-xl
+           bg-cover bg-center items-center justify-center flex"
+          >
+            <img
+              className="max-w-64 transition duration-300 hover:scale-105 
+          cursor-pointer hover:drop-shadow-2xl"
+              src={manteigasBg}
+              alt=""
+            />
           </div>
-          <div className="w-96 h-96  rounded-lg shadow-xl">
-            <h1>QUEIJOS</h1>
+          <div
+            style={{ backgroundImage: `url(${integralBg2})` }}
+            className="w-96 h-96  rounded-lg shadow-xl
+           bg-cover bg-center items-center justify-center flex"
+          >
+            <img
+              className=" transition duration-300 hover:scale-105 
+          cursor-pointer hover:drop-shadow-2xl"
+              src={leitesEmPo}
+              alt=""
+            />
           </div>
         </div>
       </section>
