75e0e8f91e495ce3a1019cd459db022be979a80e
Jose Cardoso <josedeveloperjs@gmail.com>
2024-10-13 14:57:40 -0300

    adicao scrolltools

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Components/Header";
+import ScrollToTop from "../Components/ScrollTools";
 
 export function Layout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
     </>
